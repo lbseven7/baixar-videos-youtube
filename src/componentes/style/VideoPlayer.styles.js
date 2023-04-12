f529f775486732d7728663dd0d4c6ad1f4f39295
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-11 23:13:10 -0300

    style: cria estilo para componente VideoPlayer

---
+++ src/componentes/style/VideoPlayer.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const VideoContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  background: #191919;
+  
+  h2 {
+    font-size: 2rem;
+    color: #6a6a6a;
+  }
+`;
