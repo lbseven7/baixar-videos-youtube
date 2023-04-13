3e95863b3c89e96373be03535a25fbfdac972cb9
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-13 14:27:40 -0300

    feat: cria componente Header

---
+++ src/componentes/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header(props) { 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { searchInput } = event.target.elements;
+    props.handleSearch(searchInput.value);
+  }
+
+  return (
+    <header>
+      <form onSubmit={handleSubmit}>
+        <input type="submit" placeholder='Buscar vídeos' name='searchInput' />
+        <button type='submit'>Buscar</button>
+      </form>
+    </header>
+  )
+}
+
+export default Header;
