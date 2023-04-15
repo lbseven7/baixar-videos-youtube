d04be353827a3bdbdc2f93c02090f949054a5cb1
lbseven7 <leobarbosapinturas@gmail.com>
2023-04-15 20:24:07 -0300

    chore: remove comentários desnecessários

--- src/App.js
+++ src/App.js
@@ -1,150 +1,9 @@
-// import React from 'react';
-// import VideoPlayer from './componentes/VideoPlayer.js';
-
-// function App() {
-//   const videoId = {
-//     video1: 'gepG3nMl-yM',
-//     video2: 'VUVPkoByTn4',
-//     video3: 'jSSlJ6iQCGY'
-//   };
-  
-//   const apiKey = process.env.REACT_APP_API_KEY;
- 
-
-//   return (
-//     <div>
-//        <VideoPlayer videoId={videoId.video1} apiKey={apiKey} />
-//        <VideoPlayer videoId={videoId.video2} apiKey={apiKey} />
-//        <VideoPlayer videoId={videoId.video3} apiKey={apiKey} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { useState } from 'react';
-// import VideoPlayer from './componentes/VideoPlayer.js';
-
-// function App() {
-//   const [videoId, setVideoId] = useState('');
-
-//   const apiKey = process.env.REACT_APP_API_KEY;
-
-//   const handleVideoIdChange = (event) => {
-//     setVideoId(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" value={videoId} onChange={handleVideoIdChange} />
-//       {videoId ? (
-//         <VideoPlayer videoId={videoId} apiKey={apiKey} />
-//       ) : (
-//         <div>Insira o ID do vídeo</div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import React, { useState } from 'react';
-// import VideoPlayer from './componentes/VideoPlayer.js';
-
-// function App() {
-//   const [videoId, setVideoId] = useState('');
-//   const [video, setVideo] = useState(null);
-
-//   const apiKey = process.env.REACT_APP_API_KEY;
-
-//   const handleVideoIdChange = (event) => {
-//     const newVideoId = event.target.value;
-
-//     // Atualiza o estado do ID do vídeo.
-//     setVideoId(newVideoId);
-
-//     // Verifica se o novo ID de vídeo é válido (comprimento de 11 caracteres).
-//     if (newVideoId.length !== 11) {
-//       return;
-//     }
-
-//     // Atualiza o estado do vídeo.
-//     setVideo(
-//       <VideoPlayer
-//         videoId={newVideoId}
-//         apiKey={apiKey}
-//         setVideo={setVideo}
-//       />
-//     );
-//   };
-
-//   return (
-//     <div>
-//       <input type="text" value={videoId} onChange={handleVideoIdChange} />
-//       {video || <p>Insira o ID do vídeo acima para visualizar o vídeo.</p>}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import React, { useState } from 'react';
-// import VideoPlayer from './componentes/VideoPlayer/VideoPlayer.jsx';
-// import { VideoContainer } from './componentes/VideoPlayer/style/VideoPlayer.styles.jsx';
-
-// function App() {
-//   const [videoId, setVideoId] = useState('');
-//   const [videos, setVideos] = useState([]);
-//   const apiKey = process.env.REACT_APP_API_KEY;
-
-//   const handleVideoIdChange = (event) => {
-//     const newVideoId = event.target.value;
-
-//     // Atualiza o estado do ID do vídeo.
-//     setVideoId(newVideoId);
-
-//     // Verifica se o novo ID de vídeo é válido (comprimento de 11 caracteres).
-//     if (newVideoId.length !== 11) {
-//       return;
-//     }
-
-//     // Adiciona o novo vídeo à lista de vídeos.
-//     setVideos((prevVideos) => [
-//       ...prevVideos,
-//       <VideoPlayer videoId={newVideoId} apiKey={apiKey} />
-//     ]);
-//   };
-
-//   return (
-//     <VideoContainer>
-//       <input
-//         type="text"
-//         value={videoId}
-//         onChange={handleVideoIdChange}
-//         placeholder='Insira o ID do vídeo'
-//       />
-//       {videos.length === 0 ? (
-//         <p>Insira o ID do vídeo acima para visualizar o vídeo.</p>
-//       ) : (
-//         videos.map((video, index) => (
-//           <div key={index}>
-//             {video}
-//             <hr />
-//           </div>
-//         ))
-//       )}
-//       </VideoContainer>
-//   );
-// }
-
-// export default App;
+/* eslint-disable no-undef */
 
 import React, { useState } from 'react';
 import VideoPlayer from './componentes/VideoPlayer/VideoPlayer.jsx';
 import { VideoContainer } from './componentes/VideoPlayer/style/VideoPlayer.styles.jsx';
-import Header from './componentes/Header/index.jsx';
+// import Header from './componentes/Header/index.jsx';
 
 function App() {
   const [videoId, setVideoId] = useState('');
@@ -169,17 +28,27 @@ function App() {
     ]);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { searchInput } = event.target.elements;
+    handleSearch(searchInput.value);
+  }
+
   const searchInput = () => (
-    <input
+    <>
+      <input
+      onSubmit={handleSubmit}
       type="text"
       value={videoId}
       onChange={handleVideoIdChange}
-      placeholder='Insira o ID do vídeo'
-    />
+      placeholder='Insira o ID do vídeo' />
+      {/* <button onClick={handleSubmit}>Buscar</button> */}
+    </>
   );
 
   return (
     <VideoContainer>
+      {/* <Header /> */}
       {searchInput()}
       {videos.length === 0 ? (
         <p>Insira o ID do vídeo acima para visualizar o vídeo.</p>
